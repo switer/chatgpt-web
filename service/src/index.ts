67d13714bbc4d52a4f69e8fbfd5c1a10ae5d0ca9
guankaishe <guankaishe@gmail.com>
2023-03-20 14:39:53 +0800

    use port

--- service/src/index.ts
+++ service/src/index.ts
@@ -76,4 +76,4 @@ router.post('/verify', async (req, res) => {
 app.use('', router)
 app.use('/api', router)
 
-app.listen(3002, () => globalThis.console.log('Server is running on port 3002'))
+app.listen(process.env.PORT || 3002, () => globalThis.console.log('Server is running on port 3002'))
